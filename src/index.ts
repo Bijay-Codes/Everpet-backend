56d3163ac4e93ce8547106a56eb8b4aade684376
Bijay <bijayprojects@gmail.com>
2026-09-12 12:55:49 +0530

    Use environment variable for server port, for deployment in render

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import everpet from "./app.js";
 
-everpet.listen(3000, () => {
-    console.log('Everpet-backend is running at port \n http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
 
+everpet.listen(PORT, () => {
+    console.log(`Everpet-backend is running at port \n http://localhost:${PORT}`);
+});
