08193da36bac83ada86d6068756d04f1b48b2c65
Bijay-Codes <bijayprojects@gmail.com>
2026-09-12 18:15:40 +0530

    security pathes, added throttling behavior for api to avoid DOS attacks and module to make response and request headers protected (helmet library)

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,49 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth-routes.js';
 import petRoutes from './routes/pets-routes.js';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+
 const everpet = express();// initializing
-everpet.use(cors({ origin: process.env.FRONTEND_URL }));// allows us to trust our frontend url so we can connect to it only no one else can connect
-everpet.use(express.json());
-// all the request received are parsed already, for direct use from here - whereever this milddleware is called
+
+everpet.set('trust proxy', 1);
+everpet.use(helmet());
+
+// Limiteres __ Throttling behavior 
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5,
+    message: { err: 'Too many attempts, please try again later' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+export const refreshLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 30,
+    message: { err: 'Too many requests, please try again later' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const globalLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 300, // !generous — NOT FIXED I WILL CHANGE LATER
+    message: { err: 'Too many requests, please try again later' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+everpet.use(globalLimiter);
+
+if (!process.env.FRONTEND_URL) {
+    throw new Error('FRONTEND_URL environment variable is not set');
+}
+
+everpet.use(cors({ origin: process.env.FRONTEND_URL })); // allows us to trust our frontend url so we can connect to it only no one else can connect
+
+everpet.use(express.json()); // all the request received are parsed already, for direct use from here - whereever this milddleware is called
+
 
 
 everpet.use('/auth', authRoutes);// login and signup
--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 import login from "../controllers/auth-controller/login-controller.js";
 import register from "../controllers/auth-controller/register-controller.js";
 import refresh from "../controllers/auth-controller/refresh-controller.js";
-
+import { refreshLimiter, authLimiter } from "../app.js";
 const authRoutes = Router();
 
-authRoutes.post('/register', register);
-authRoutes.post('/login', login);
-authRoutes.post('/refresh', refresh);
+authRoutes.post('/register', authLimiter, register);
+authRoutes.post('/login', authLimiter, login);
+authRoutes.post('/refresh', refreshLimiter, refresh);
 
 export default authRoutes;
